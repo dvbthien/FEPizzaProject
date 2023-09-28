bf7c998331f4e088511dc5138dce9fd57aa52672
Tuan Nguyen <minhtuannuyen140397@gmail.com>
2023-09-28 16:09:35 +0700

    add location menu section

--- src/app/_components/home-page/page.tsx
+++ src/app/_components/home-page/page.tsx
@@ -1,9 +1,12 @@
 import Banner from './Banner'
+import LocationMenu from './LocationMenu'
 
 export default function Homepage() {
     return (
         <>
             <Banner />
+            <LocationMenu />
         </>
+
     )
 }
