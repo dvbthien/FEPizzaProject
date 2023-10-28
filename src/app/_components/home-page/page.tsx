beafa257502388fece9a12ffea391076b8f26fbf
Tuan Nguyen <minhtuannuyen140397@gmail.com>
2023-10-28 22:33:06 +0700

    implement social media and vision section

--- src/app/_components/home-page/page.tsx
+++ src/app/_components/home-page/page.tsx
@@ -1,11 +1,15 @@
 import Banner from './Banner'
 import LocationMenu from './LocationMenu'
+import SocialMediaSection from './SocialMediaSection'
+import VisionSection from './VisionSection'
 
 export default function Homepage() {
     return (
         <>
             <Banner />
             <LocationMenu />
+            <SocialMediaSection />
+            <VisionSection />
         </>
 
     )
